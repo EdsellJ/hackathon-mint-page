183d72c79c25bf788b2c62a0d75b2841a5e273f3
BisiDavi <oludavidconnect@gmail.com>
2023-02-11 12:05:22 +0100

    job-board updated

--- frontend/src/components/JobCard.tsx
+++ frontend/src/components/JobCard.tsx
@@ -2,39 +2,54 @@ import Link from "next/link";
 
 import { getJobPostDate } from "utils/getTimeDifference";
 import toSlug from "utils/toSlug";
-import type { jobType } from "@types";
+import type { jobType } from "types";
 
 interface Props {
-	job: jobType;
+  job: jobType;
 }
 
 export default function JobCard({ job }: Props) {
-	const jobSlug = toSlug(`${job.title}-at-${job.company}`);
-	const postedDaysAgo = getJobPostDate(job.date);
-	return (
-		<Link href={`/job-board/${jobSlug}`}>
-			<div className="jobcard border h-52 bg-white border-gray rounded-xl py-5 px-4 hover:bg-lightgrey">
-				<div className="top flex items-start justify-between">
-					<div className="w-full">
-						<h5 className="text-lg text-primary font-bold truncate">{job.title}</h5>
-						<p className="font-light text-primary mt-2">{job.company}</p>
-						<ul className="space-x-3 w-full flex overflow-scroll  mt-4">
-							{job.tags.map((item) => (
-								<li
-									key={item}
-									className="rounded-full my-1 whitespace-nowrap text-primary font-light border border-blue px-2 hover-bg-light-blue"
-								>
-									{item}
-								</li>
-							))}
-						</ul>
-					</div>
-				</div>
-				<div className="bottom mt-6 flex justify-between items-center">
-					<p className="text-primary font-thin">{postedDaysAgo}</p>
-					<img src="/images/bookmark.png" alt="bookmark this job" className="w-3.5" />
-				</div>
-			</div>
-		</Link>
-	);
+  const jobSlug = toSlug(`${job.title}-at-${job.company}`);
+  const postedDaysAgo = getJobPostDate(job.date);
+  return (
+    <Link href={`/job-board/${jobSlug}`}>
+      <div className="jobcard border h-52 bg-white rounded-4 border-gray py-5 px-4 hover:bg-lightgrey">
+        <div className="top flex items-start justify-between">
+          <div className="w-full">
+            <h5 className="text-lg text-primary font-bold truncate">
+              {job.title}
+            </h5>
+            <p className="font-light text-primary mt-2">{job.company}</p>
+            <ul className="gap-4 list-unstyled d-flex overflow-scroll mt-4">
+              {job.tags.map((item: string) => (
+                <li
+                  key={item}
+                  className="rounded-pill my-1 whitespace-nowrap text-deep-blue font-light border px-5"
+                >
+                  {item}
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+        <div className="bottom mt-6 flex justify-between items-center">
+          <p className="text-primary font-thin">{postedDaysAgo}</p>
+          <img
+            src="/images/bookmark.png"
+            alt="bookmark this job"
+            className="w-3.5"
+          />
+        </div>
+      </div>
+      <style jsx>{`
+        ul.list-unstyled {
+          overflow-x: scroll;
+        }
+        ul.list-unstyled li {
+          text-decoration: none;
+          underline: none;
+        }
+      `}</style>
+    </Link>
+  );
 }
--- frontend/src/components/JobGrid.tsx
+++ frontend/src/components/JobGrid.tsx
@@ -3,34 +3,43 @@ import JobCardLoader from "components/JobCardLoader";
 import type { jobType } from "types";
 
 interface Props {
-	scroll: number;
-	jobs: Array<jobType>;
-	status: "success" | "loading" | "error";
+  scroll: number;
+  jobs: Array<jobType>;
+  status: "success" | "loading" | "error";
 }
 
 export default function JobGrid({ scroll, jobs, status }: Props) {
-	const jobGridPosition =
-		Number(scroll) > 300 ? "absolute right-0 w-9/12 pl-14" : "w-9/12";
+  const jobGridPosition =
+    Number(scroll) > 300 ? "position-absolute right-0 col-10  pl-14" : "col-9";
 
-	return (
-		<div className={`jobgrid ${jobGridPosition} pb-10 flex flex-col`}>
-			<div className="wrapper mx-auto justify-center flex">
-				{status === "error" ? (
-					<p>Unable to fetch job due to an error</p>
-				) : status === "loading" ? (
-					<JobCardLoader />
-				) : jobs.length > 0 ? (
-					<div className="grid gap-x-8 gap-y-8 grid-cols-2 w-full">
-						{jobs.map((job) => (
-							<JobCard key={job.id} job={job} />
-						))}
-					</div>
-				) : (
-					<h4 className="font-bold flex mx-auto justify-center text-center">
-						Oops, No job found for this search query
-					</h4>
-				)}
-			</div>
-		</div>
-	);
+  return (
+    <div className={`jobgrid ${jobGridPosition} pb-10 flex flex-col`}>
+      <div className="wrapper mx-auto justify-center flex">
+        {status === "error" ? (
+          <p>Unable to fetch job due to an error</p>
+        ) : status === "loading" ? (
+          <JobCardLoader />
+        ) : jobs.length > 0 ? (
+          <div className="grid gap-x-8 gap-y-8 grid-cols-2 w-full">
+            {jobs.map((job) => (
+              <JobCard key={job.id} job={job} />
+            ))}
+          </div>
+        ) : (
+          <h4 className="font-bold flex mx-auto justify-center text-center">
+            Oops, No job found for this search query
+          </h4>
+        )}
+      </div>
+      <style jsx>
+        {`
+          .grid {
+            display: grid;
+            grid-template-columns: repeat(2, 1fr);
+            gap: 40px;
+          }
+        `}
+      </style>
+    </div>
+  );
 }
