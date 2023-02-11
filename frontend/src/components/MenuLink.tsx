a41d1430a09d5972b0f25b7ea53ef2c8f61b95a6
BisiDavi <oludavidconnect@gmail.com>
2023-02-11 11:07:18 +0100

    NFTHeroBanner updated

--- frontend/src/components/MenuLink.tsx
+++ frontend/src/components/MenuLink.tsx
@@ -2,26 +2,34 @@ import Link from "next/link";
 import { useRouter } from "next/router";
 
 interface Props {
-	menu: {
-		link: string;
-		text: string;
-	};
+  menu: {
+    link: string;
+    text: string;
+  };
 }
 
 export default function MenuLink({ menu }: Props) {
-	const router = useRouter();
-	const activeStyle = {
-		padding: "8px",
-		borderBottom: "3px solid black",
-	};
+  const router = useRouter();
 
-	const styles = router.asPath.includes(menu.link) ? activeStyle : {};
+  const activeClassName = router.asPath.includes(menu.link) ? "active" : "";
 
-	return (
-		<li className="text-decoration-none mx-3">
-			<Link href={menu.link} className="text-decoration-none fs-5 text-black" style={styles}>
-				{menu.text}
-			</Link>
-		</li>
-	);
+  return (
+    <>
+      <li className={`text-decoration-none mx-3 ${activeClassName}`}>
+        <Link href={menu.link} className="text-decoration-none fs-5">
+          <span>{menu.text}</span>
+        </Link>
+      </li>
+      <style jsx>{`
+        li.active span {
+          padding: 8px;
+          border-bottom: 3px solid #404471;
+          color: #404471;
+        }
+        li span {
+          color: black;
+        }
+      `}</style>
+    </>
+  );
 }
--- frontend/src/components/NFTHeroBanner.tsx
+++ frontend/src/components/NFTHeroBanner.tsx
@@ -2,26 +2,26 @@ import Button from "components/Button";
 
 export default function NFTHeroBanner() {
   return (
-    <section className="container mt-2">
-      <div className="flex items-center px-16">
-        <div className="text-content left w-1/2">
-          <h2 className="text-xl text-primary my-2">EduCoin Badge Creator</h2>
-          <p>
-            By clicking “Mint a Badge” you are creating a non-fungible token
-            which can be used to certify skills on the NEAR blockchain.
-            <br />
-            <br />
-            This token can be used to check references and validate proof of
-            work and learned skills.
-          </p>
-          <Button
-            text="Mint a Badge"
-            className="rounded-full font-bold bg-primary py-3 text-white border px-8 text-secondary-500 mt-6 hover-bg-primary"
-          />
-        </div>
-        <div className="image w-1/2">
-          <img src="/assets/woman.png" alt="woman minting nft" />
-        </div>
+    <section className="container d-flex  align-items-center gap-4 mt-4 mb-5">
+      <div className="col-6">
+        <h2 className="fs-1 fw-bold text-deep-blue my-2">
+          EduCoin Badge Creator
+        </h2>
+        <p>
+          By clicking “Mint a Badge” you are creating a non-fungible token which
+          can be used to certify skills on the NEAR blockchain.
+          <br />
+          <br />
+          This token can be used to check references and validate proof of work
+          and learned skills.
+        </p>
+        <Button
+          text="Mint a Badge"
+          className="rounded-pill fw-bold bg-deep-blue py-3 text-white border px-5 text-secondary-500 mt-4"
+        />
+      </div>
+      <div className="image col-6">
+        <img src="/assets/woman.png" alt="woman minting nft" />
       </div>
     </section>
   );
