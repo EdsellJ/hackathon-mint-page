7a10626a6e3647f1f42e333be3aa1db03399193c
BisiDavi <oludavidconnect@gmail.com>
2023-02-11 01:58:34 +0100

    page updated

--- frontend/src/pages/api/jobs.ts
+++ frontend/src/pages/api/jobs.ts
@@ -1,13 +1,16 @@
 import axios from "axios";
 import type { NextApiRequest, NextApiResponse } from "next";
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-	switch (req.method) {
-		case "GET": {
-			const result = await axios.get(
-				` ${process.env.WEB3_API}?token=${process.env.WEB3_API_TOKEN}`
-			);
-			return res.status(200).send(result.data);
-		}
-	}
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  switch (req.method) {
+    case "GET": {
+      const result = await axios.get(
+        "https://web3.career/api/v1?token=A8qGWD3bCZi2jwyrRGBYGfDenw3bjbQA"
+      );
+      return res.status(200).send(result.data);
+    }
+  }
 }
--- frontend/src/utils/getEvents.ts
+++ frontend/src/utils/getEvents.ts
@@ -1,12 +1,12 @@
 import axios from "axios";
 
 export async function getEventBriteEventList() {
-	return await axios.get(
-		`${process.env.NEXT_PUBLIC_EVENTBRITE_BASE_API}/organizations/${process.env.NEXT_PUBLIC_EVENTBRITE_ORG_ID}/events/`,
-		{
-			headers: {
-				Authorization: `Bearer ${process.env.NEXT_PUBLIC_EVENTBRITE_PERSONAL_AUTH_TOKEN}`,
-			},
-		}
-	);
+  return await axios.get(
+    "https://www.eventbriteapi.com/v3/organizations/892957692073/events/",
+    {
+      headers: {
+        Authorization: "Bearer TTWE2VOCBIDD4O7BU44Y",
+      },
+    }
+  );
 }
