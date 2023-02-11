0530982336250d0dad9e0f145d810bb15515d6ea
BisiDavi <oludavidconnect@gmail.com>
2023-02-11 11:20:19 +0100

    eventbritelisting added

--- frontend/src/components/UpcomingEvents.tsx
+++ frontend/src/components/UpcomingEvents.tsx
@@ -4,6 +4,7 @@ import Logo from "components/Logo";
 import SpinnerRipple from "components/SpinnerRipple";
 import { getEventBriteEventList } from "utils/getEvents";
 import { formatDate } from "utils/getTimeDifference";
+import EventBriteListing from "./EventBriteListing";
 
 export default function UpcomingEvents() {
   const { data, status } = useQuery(["list-event"], getEventBriteEventList);
@@ -11,7 +12,7 @@ export default function UpcomingEvents() {
   const eventArray = status === "success" ? data.data.events.reverse() : [];
 
   return (
-    <div className="my-8 upcoming-events px-16 text-primary mx-auto flex flex-col justify-center  container">
+    <div className="my-5 upcoming-events text-deep-blue mx-auto flex flex-col justify-center  container">
       <h2>Upcoming Events</h2>
       <p>Earn badges at upcoming events!</p>
       <div className="span w-1/6 my-4">
@@ -22,38 +23,10 @@ export default function UpcomingEvents() {
       ) : status === "loading" ? (
         <SpinnerRipple centerRipple />
       ) : (
-        <ul className="">
-          {eventArray.map((briteEvent: any) => {
-            const eventStartDate = formatDate(briteEvent.start.utc);
-            const eventEndDate = formatDate(briteEvent.end.utc);
-            return (
-              <li key={briteEvent.id} className="my-6">
-                <img
-                  src={briteEvent.logo.original.url}
-                  height={`${briteEvent.logo.crop_mask.height}px`}
-                  width={`${briteEvent.logo.crop_mask.width}px`}
-                  alt={briteEvent.name.text}
-                  className="my-6"
-                />
-                <h2 className="font-bold my-4">{briteEvent.name.text}</h2>
-                <p>{briteEvent.description.text}</p>
-                <div className="bottom-info my-8 flex justify-between">
-                  <a
-                    href={briteEvent.url}
-                    target="_blank"
-                    rel="noreferrer"
-                    className="text-red-500 underline"
-                  >
-                    Vist Event ({briteEvent.is_free ? "free" : "paid"})
-                  </a>
-                  <div className="date flex flex-col">
-                    <span>Start : {eventStartDate}</span>
-                    <span>End : {eventEndDate}</span>
-                  </div>
-                </div>
-              </li>
-            );
-          })}
+        <ul className="list-unstyled d-flex w-100 gap-4">
+          {eventArray.map((briteEvent: any) => (
+            <EventBriteListing key={briteEvent.id} briteEvent={briteEvent} />
+          ))}
         </ul>
       )}
     </div>
--- frontend/src/types/index.ts
+++ frontend/src/types/index.ts
@@ -1,10 +1,23 @@
 export type jobType = {
-	id: string;
-	date: Date;
-	title: string;
-	tags: string[];
-	description: string;
-	company: string;
-	apply_url: string;
-	location: string;
+  id: string;
+  date: Date;
+  title: string;
+  tags: string[];
+  description: string;
+  company: string;
+  apply_url: string;
+  location: string;
+};
+
+export type briteEventType = {
+  logo: {
+    original: { url: string };
+    crop_mask: { height: number; width: number };
+  };
+  start: { utc: string };
+  end: { utc: string };
+  description: { text: string };
+  name: { text: string };
+  url: string;
+  is_free: boolean;
 };
